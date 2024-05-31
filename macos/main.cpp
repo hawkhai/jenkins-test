9d5e7f5428ff8884bcfac010597c1dc5bdf23612
hawkhai <haihoing@qq.com>
2024-05-31 16:58:42 +0800

    macos & qt5

---
+++ macos/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <QApplication>
+#include <QPushButton>
+#include <QtNetwork>
+
+int main(int argc, char *argv[]) {
+
+    auto temp1 = QSslSocket::sslLibraryBuildVersionString();
+    auto temp2 = QSslSocket::supportsSsl();
+
+    QNetworkAccessManager *manager = new QNetworkAccessManager(nullptr);
+    auto temp3 = manager->supportedSchemes().join("/");
+
+    QApplication a(argc, argv);
+    QPushButton button(temp1 + "\n" + temp3, nullptr);
+    button.resize(200, 100);
+    button.show();
+    return QApplication::exec();
+}
